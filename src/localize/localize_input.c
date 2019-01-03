aed8561e46953804fe9aab6592a73757216ea1df
KK-MS <KK-MS@github.com>
2019-01-03 17:30:01 +0100

    Add input and output template files

---
+++ src/localize/localize_input.c
@@ -0,0 +1,17 @@
+/*
+ * File: localize_input.c
+ *
+ * The input interface of localization algorithm.
+ * INPUT:  Timestamp, IMU, SatGPS, Stereo 
+ * OUTPUT: Timestamp, LoGPS
+ *   Note: SaGPS: Satellite GPS coordinates
+ *         LoGPS: Localized GPS coordinates
+ *         GtGPS: Ground truth map GPS coordinates 
+ */
+
+int localize_input_init()
+{
+    // To be implemented
+    return 0;
+}
+
---
+++ src/localize/localize_output.c
@@ -0,0 +1,17 @@
+/*
+ * File: localize_output.c
+ *
+ * The output interface of localization algorithm.
+ * INPUT:  Timestamp, IMU, SatGPS, Stereo 
+ * OUTPUT: Timestamp, LoGPS
+ *   Note: SaGPS: Satellite GPS coordinates
+ *         LoGPS: Localized GPS coordinates
+ *         GtGPS: Ground truth map GPS coordinates 
+ */
+
+int localize_output_init()
+{
+    // To be implemented
+    return 0;
+}
+
